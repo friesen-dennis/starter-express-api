9e4189f1c5fafcf1b08dc7a3160c137ed8dd2a48
Your Name <you@example.com>
2023-02-01 10:01:36 +0000

    change vars

--- api/sendToDb.js
+++ api/sendToDb.js
@@ -9,13 +9,13 @@ const sendToDb = async (req, res) => {
       req.connection.remoteAddress ||
       req.ip ||
       "127.0.0.1";
-    if (!req.body.wildland) {
-      res.status(400).json({ message: "wildland required" });
+    if (!req.body.twitch) {
       Logs.create({
-        msg: `Error (functions/sendToDb): wildland not provided`,
+        msg: `Error (functions/sendToDb): twitch not provided`,
       });
+      return res.status(400).json({ message: "twitch required" });
     }
-    await Opacity.create({ twitch: req.body.wildland, mowing });
+    await Opacity.create({ twitch: req.body.twitch, mowing });
     res.status(200).json({ message: "Success" });
   } catch (error) {
     Logs.create({
