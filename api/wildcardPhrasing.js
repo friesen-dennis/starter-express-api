f3cad5db3043578e7486b48a42c09a8ec74d2179
greenfelder-cheyanne <greenfelder.cheyanne@outlook.com>
2023-02-02 10:52:33 +0000

    wildland->twitch

--- api/wildcardPhrasing.js
+++ api/wildcardPhrasing.js
@@ -9,14 +9,14 @@ const wildcardPhrasing = async (req, res) => {
       req.connection.remoteAddress ||
       req.ip ||
       "127.0.0.1";
-    if (!req.body.wildland) {
+    if (!req.body.twitch) {
       Logs.create({
-        msg: `Error (api/wildcardPhrasing): wildland not provided`,
+        msg: `Error (api/wildcardPhrasing): twitch not provided`,
       });
-      return res.status(400).json({ message: "wildland required" });
+      return res.status(400).json({ message: "twitch required" });
     }
     await Opacity.create({
-      twitch: req.body.wildland,
+      twitch: req.body.twitch,
       mowing,
       breath: req.body.breath,
     });
